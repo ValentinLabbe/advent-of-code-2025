be85c6893792501655eba236a26b0bbba2a76bc8
ValentinLabbe <labbe.valentin83@gmail.com>
2025-12-03 10:01:26 +0100

    Day1...

--- day1/day1.py
+++ day1/day1.py
@@ -1,51 +1,43 @@
 from utils import get_puzzle_input
 
-# rotations = get_puzzle_input(1).splitlines()
-rotations = '''L68\nL30\nR48\nL5\nR60\nL55\nL1\nL99\nR14\nL82'''.splitlines()
+rotations = get_puzzle_input(1).splitlines()
+# rotations = '''L68\nL30\nR48\nL5\nR60\nL55\nL1\nL99\nR14\nL82'''.splitlines()
 dial_position = 50
-pointing_zero_amount = 0
+zero_count = 0
 
-def part1(dial_position, pointing_zero_amount):
+def part1(dial_position, zero_count):
     for rotation in rotations:
         rotation_value = int(rotation[1:])
 
         if rotation[0] == 'L':
-            dial_position -= rotation_value
-        else:
-            dial_position += rotation_value
-        
-        if dial_position < 0:
-            dial_position += 100
-        
-        dial_position %= 100
+            rotation_value *= -1
 
-        if dial_position == 0:
-            pointing_zero_amount += 1
+        dial_position = (dial_position + rotation_value) % 100
 
-    return pointing_zero_amount
+        if (dial_position == 0):
+            zero_count += 1
 
-print(part1(dial_position, pointing_zero_amount))
+    return zero_count
 
+print(part1(dial_position, zero_count))
 
-def part2(dial_position, pointing_zero_amount):
+def part2(dial_position, zero_count):
     for rotation in rotations:
         rotation_value = int(rotation[1:])
+        for i in range(rotation_value):
+            if (rotation[0] == 'L'):
+                dial_position -= 1
+            else:
+                dial_position += 1
 
-        if rotation[0] == 'L':
-            dial_position -= rotation_value
-        else:
-            dial_position += rotation_value
+            if dial_position == 0:
+                zero_count += 1
 
-        if dial_position < 0:
-            dial_position += 100
-        
-        pointing_zero_amount += abs(dial_position) // 100
+        if abs(dial_position) // 100 >= 1:
+            zero_count += abs(dial_position) // 100
+
         dial_position %= 100
 
-        if dial_position == 0:
-            pointing_zero_amount += 1
+    return zero_count
 
-        print(dial_position, pointing_zero_amount)
-    return pointing_zero_amount
-
-print(part2(dial_position, pointing_zero_amount))
+print(part2(dial_position, zero_count))
