27cb39fdd3de4713da33b60c213bed2f5c741280
ValentinLabbe <labbe.valentin83@gmail.com>
2025-12-05 21:11:06 +0100

    d4p2

--- day4/day4.py
+++ day4/day4.py
@@ -63,16 +63,36 @@ def get_adjacent_positions(point: Point):
 
     return positions
 
-def get_accessible_paper_rolls_sum():
-    accessible_paper_rolls = 0
+def get_accessible_paper_rolls():
+    accessible_paper_rolls = []
 
-    for line_index in range(len(paper_roll_map[0])):
+    for row_index in range(len(paper_roll_map[0])):
         for column_index in range(len(paper_roll_map)):
-            point = Point(int(line_index), int(column_index))
+            point = Point(int(row_index), int(column_index))
             if (is_paper_roll(point)):
                 if (is_accessible_by_forklift(point)):
-                    accessible_paper_rolls += 1
+                    accessible_paper_rolls.append(point)
     
     return accessible_paper_rolls
 
-print(get_accessible_paper_rolls_sum())
+print(len(get_accessible_paper_rolls()))
+
+# Part 2
+def remove_rolls(points: list[Point]):
+    global paper_roll_map
+
+    for point in points:
+        paper_roll_map[point.x] = paper_roll_map[point.x][:point.y] + '.' + paper_roll_map[point.x][point.y + 1:]
+
+def get_all_accessible_paper_rolls_sum():
+    accessible_paper_rolls_sum = 0
+    accessible_paper_rolls = get_accessible_paper_rolls()
+
+    while(len(accessible_paper_rolls) != 0):
+        accessible_paper_rolls_sum += len(accessible_paper_rolls)
+        remove_rolls(accessible_paper_rolls)
+        accessible_paper_rolls = get_accessible_paper_rolls()
+
+    return accessible_paper_rolls_sum
+
+print(get_all_accessible_paper_rolls_sum())
